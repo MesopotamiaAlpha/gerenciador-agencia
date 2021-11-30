28b3e7f5b5d5d4af0abf19a306a28ea617beb1c5
Jose antonio <leao-gomes@hotmail.com>
2021-11-30 18:52:55 -0300

    Ajustes

--- src/components/prinex/resultado.js
+++ src/components/prinex/resultado.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./resultado";
 import "./resultado.css";
 import Axios from "axios";
-import { Button } from '@material-ui/core';
 
+// esta função traz todos os registros dentro do banco de dados e usa a função map para criar um array de objetos
 function Resultado_Pri() {
     const [postList, setPostList] = useState([]);
 
@@ -15,26 +15,23 @@ function Resultado_Pri() {
             
         });
     }, [])
-//Parte de cima é para pegar os posts e listar em map
-
-//parte de baixo vai ser para delet dos posts mapeados
 
 
-const [post,setPost] = useState({});
+//Na parte de baixo ele usa para apagar os registros do banco de dados
 
 const deletePost = (id) => {
     Axios.delete(`http://192.168.1.27:3002/api/delete/${id}`).then((response)=>{
         alert("you deleted a post")
     })
 }
-//em cima é a função para deletar o post
-    
+
+// dentro do return ele verifica o array e cria um botão para deletar para cada registro dentro do banco de dados, os botoes possuem um id de cada registro para apagar cada um    
     return (
         <div className="resultado">
                 <h1 className="titulo">Prinex recebidos</h1>
-                <ul className="textoLista">
-                    {postList.map(nome_prinex => <div key={nome_prinex.id_pri}>{nome_prinex.nome_prinex}
-                        <Button size="small" color="error" variant="outlined" onClick={(() => deletePost(nome_prinex.id_pri))}>X</Button>
+                <ul>
+                    {postList.map(nome_prinex => <div className="textoLista" key={nome_prinex.id_pri}>{nome_prinex.nome_prinex}
+                        <button className="btnDel" onClick={(() => deletePost(nome_prinex.id_pri))}>X</button>
                     </div>)}
                     
                 </ul>
