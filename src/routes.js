700eb95691fae080a35c2e213cd70b7a4e700c60
Jose antonio <leao-gomes@hotmail.com>
2021-12-21 02:41:46 -0300

    Adição da rota para pendencias

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import SearchAppBar from './components/topBar'
 import Cadastro_Pri from './components/prinex/cadastro_pri';
 import FechamentoCaixa from './components/fechamentoCaixa/fechamentoCaixa';
 import Agendamento from './components/agendamento/agendamento';
+import Estoque from './components/estoque/estoque';
+import Pendencias from './components/pendencias/pendencias';
 
 
 export default function MainRoutes() {
@@ -14,12 +16,15 @@ export default function MainRoutes() {
             <Link to="/home">Pagina principal</Link>
             <Link to="/fechamentoCaixa">Fechamento Caixa</Link>
             <Link to="/agendamento">Agendamento de passageiros</Link>
+            <Link to="/estoque">Estoque</Link>
+            <Link to="/pendencias">Pendencias passageiros</Link>
             <Routes>
                 <Route path='/' element={<Cadastro_Pri />} />
                 <Route path='/home' element={<Cadastro_Pri />} />
                 <Route path='/agendamento' element={<Agendamento />} />
                 <Route path='/fechamentoCaixa' element={<FechamentoCaixa />} />
-
+                <Route path='/estoque' element={<Estoque />} />
+                <Route path='/pendencias' element={<Pendencias />} />
             </Routes>
 
         </Router>
